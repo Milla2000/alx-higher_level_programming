259e1d390fa5e36d4596162c69997965b1264941
Milla2000 <millajesso2000@gmail.com>
2023-06-09 10:02:19 +0300

    correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
-#!/usr/bin/python3
-
 import sys
 
-count = len(sys.argv) - 1
-if count == 0:
+args = sys.argv[1:]  # Exclude the script name from the arguments
+
+num_args = len(args)
+
+if num_args == 0:
     print("0 arguments.")
-elif count == 1:
-    print("1 argument:")
 else:
-    print("{} arguments:".format(count))
-for i in range(count):
-     print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    print("{} argument{}:".format(num_args, 's' if num_args > 1 else ''))
+    for i, arg in enumerate(args):
+        print("{}: {}".format(i + 1, arg))
